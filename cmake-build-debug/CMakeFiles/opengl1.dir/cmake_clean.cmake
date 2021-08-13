21172ac508b41aa093690737aefa6b5b9cfe26b2
mevunidesaram <mevunid97@gmail.com>
2021-08-13 12:48:19 +0530

    test1

---
+++ cmake-build-debug/CMakeFiles/opengl1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl1.dir/main.cpp.o"
+  "opengl1"
+  "opengl1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
